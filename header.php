b5e6e8cef71b6252384fc3d2feda3d83cf17bce0
Vaibhav Biradar <vaibhavbb007@gmail.com>
2017-02-09 10:57:00 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		header{
+			background: #524a8b;
+			width: 100%;
+			padding: 10px;
+		}
+		ul li{
+			color: white;
+			font-family: Ubuntu;
+			display: inline-block;
+		}
+		a{
+			color: white;
+			font-family: Ubuntu;
+			font-size: 35px;
+		}
+		#logo{
+		height: 60px;
+		padding: 5px;
+	}
+	</style>
+</head>
+<body>
+<header>
+	<span id='head'>
+		<img src="logo.png" id="logo">
+		<ul type='none'>
+			<li><a href="/index.php">Home</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+		</ul>
+	</span>
+</header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biopath Diagnostics</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<style type="text/css">
+		body{
+			margin: 0;
+		}
+	</style>
+</head>
+<body>
+</body>
+</html>
